a001532d4d226f191c8dff8c2da0c939bdf23173
kgilbert78 <kgilbert78@gmail.com>
2023-04-21 19:36:45 -0400

    fixed leaked secret key

--- main_api/main_api/settings.py
+++ main_api/main_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3*%&mvsttwuej*7d!+#xpi7)c^o58k##pc)trsoh!8iq3%+f+='
+SECRET_KEY = config("SECRET_KEY")
+
+# generated new secret key in terminal with:
+# python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
+# https://www.educative.io/answers/how-to-generate-a-django-secretkey
+
+# created file called .env in same directory as manage.py, and put in it SECRET_KEY = ''
+# ^ new secret key in quotes above
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
