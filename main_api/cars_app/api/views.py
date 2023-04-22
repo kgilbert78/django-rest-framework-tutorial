3cb5ff1f63332e7a26dd2f7d45d45e49dac46b28
kgilbert78 <kgilbert78@gmail.com>
2023-04-22 18:18:35 -0400

    handle query params if present (and id is a number)

--- main_api/cars_app/api/views.py
+++ main_api/cars_app/api/views.py
@@ -1,8 +1,18 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.permissions import IsAuthenticated
 
 @api_view()
-@permission_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 def drive(request):
-    return Response({"message": "vroom!"})
+    # print(request.query_params) 
+        # url .../cars/?id=1 prints <QueryDict: {'id': ['1']}>
+        # url .../cars/?id=1&key=25 prints <QueryDict: {'id': ['1'], 'key': ['25']}>
+    # print(request.query_params['id']) # ^ prints 1
+    # print(request.query_params['key']) # ^ prints 25
+    id = None
+    if request.query_params:
+        params = list(request.query_params) # create list of the keys in param QueryDict
+        if 'id' in params:
+            id = int(request.query_params['id']) # convert from string for calculations
+    return Response({"message": "vroom!", "id": id, "doubled-id": id*2})
