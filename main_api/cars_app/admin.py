d735ef713e79d4114b2f62973e57a37f71d8cbb8
kgilbert78 <kgilbert78@gmail.com>
2023-04-22 18:44:34 -0400

    added & registerd class-based model, no view yet

--- main_api/cars_app/admin.py
+++ main_api/cars_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Car
 
 # Register your models here.
+admin.site.register(Car)
+
+# now it shows in the admin/ panel and you can add data there
--- main_api/cars_app/models.py
+++ main_api/cars_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    car_brand = models.CharField(max_length=30)
+    car_model = models.CharField(max_length=50)
+    car_year = models.CharField(max_length=4) # will be string
+    car_color = models.CharField(max_length=30)
+
+    # then run migrations after creating this ^
+
+    # admin interface displays entries as Car object (1), this makes it display model name
+    def __str__(self) -> str:
+        return self.car_model
