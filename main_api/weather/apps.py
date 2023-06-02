143f69e150105f5f640c8b9d98c876d178b03f11
kgilbert78 <kgilbert78@gmail.com>
2023-06-01 20:09:25 -0400

    temporarily disabled automatic calls to weather api

--- main_api/weather/apps.py
+++ main_api/weather/apps.py
@@ -5,8 +5,9 @@ class WeatherConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'weather'
 
-    def ready(self) -> None:
-        print("Starting weather scheduler...")
-        from .scheduler import updater
-        updater.start_2min_weather_updates()
+    # # uncomment to make calls to weather api every 2 minutes
+    # def ready(self) -> None:
+    #     print("Starting weather scheduler...")
+    #     from .scheduler import updater
+    #     updater.start_2min_weather_updates()
         
