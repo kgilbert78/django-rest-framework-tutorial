a57056f9542e9f609e5bcc1845aa1089c4fa4468
kgilbert78 <kgilbert78@gmail.com>
2023-06-11 20:22:35 -0400

    added throttling on entire app for user vs. non-user

--- main_api/main_api/settings.py
+++ main_api/main_api/settings.py
@@ -136,6 +136,14 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'USER_DETAILS_SERIALIZER': 'user_app.serializer.UserDetailsSerializer',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/day',
+        'user': '4/day'
+    }
 }
 
 # Internationalization
